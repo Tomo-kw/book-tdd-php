5ea929acd99c20ca3a256dc12a4883cf3dec2f0e
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2024-04-21 11:14:54 +0900

    Page_40 Francクラスをサブクラス化：6章終わり

--- src/Money/Franc.php
+++ src/Money/Franc.php
@@ -4,21 +4,10 @@ declare(strict_types=1);
 
 namespace Money;
 
-final class Franc
+final class Franc extends Money
 {
-    public function __construct(
-        private int $amount
-    )
-    {
-    }
-
     public function times(int $multiplier)
     {
         return new Franc($this->amount * $multiplier);
-    }
-
-    public function equals(Franc $object): bool
-    {
-        return $this->amount === $object->amount;
     }
 }
